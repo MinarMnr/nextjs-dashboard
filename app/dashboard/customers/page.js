dce1ac67d1d4b4f2dfc68bc64e5524be537a09e9
minar <mahmudur@eatlbd.com>
2024-09-19 11:45:19 +0600

    copied

---
+++ app/dashboard/customers/page.js
@@ -0,0 +1,9 @@
+import { MapPinIcon } from "@heroicons/react/24/solid";
+
+export default function Page() {
+  return (
+    <p>
+      Customer Page <MapPinIcon className="size-6 " />
+    </p>
+  );
+}
